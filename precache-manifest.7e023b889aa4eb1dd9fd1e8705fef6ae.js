aa4f1f423784c90bdf8bec921145102a4efb0118
Pedro Henrique <pedrotrab2132@gmail.com>
2020-04-27 00:34:43 -0300

    Updates

---
+++ precache-manifest.7e023b889aa4eb1dd9fd1e8705fef6ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc61c6bba503c41158d1dc9cc0cbc71",
+    "url": "/lar/index.html"
+  },
+  {
+    "revision": "8ccff24b7d81ecfee7d7",
+    "url": "/lar/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "8ccff24b7d81ecfee7d7",
+    "url": "/lar/static/js/2.37ac452a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/lar/static/js/2.37ac452a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d0e5f680c7fba4c886",
+    "url": "/lar/static/js/main.fad129af.chunk.js"
+  },
+  {
+    "revision": "039bbb305ed42a1739bb",
+    "url": "/lar/static/js/runtime-main.d93470f8.js"
+  },
+  {
+    "revision": "fc3ba7bf1cfa1630266e46c433212217",
+    "url": "/lar/static/media/logo-01.fc3ba7bf.png"
+  },
+  {
+    "revision": "c55e85d338dba29ca633679c01b41a71",
+    "url": "/lar/static/media/name.c55e85d3.png"
+  },
+  {
+    "revision": "0b0a90e92834bcc7a698b01a0844c5f6",
+    "url": "/lar/static/media/perfil.0b0a90e9.png"
+  }
+]);
